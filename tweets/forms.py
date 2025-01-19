714d8e6c9be08f1583e2c5b118bccdab51c233a4
jagadish-pokharel <117583302+jagadish-pokharel@users.noreply.github.com>
2025-01-19 16:00:15 +0545

    Add files via upload

---
+++ tweets/forms.py
@@ -0,0 +1,8 @@
+# # forms.py
+# from django import forms
+# from .models import UserProfile
+
+# class UserProfileForm(forms.ModelForm):
+#     class Meta:
+#         model = UserProfile
+#         fields = ['age', 'location', 'city', 'state', 'country']
